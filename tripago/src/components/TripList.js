82d4ef40227ccce9e9e09c43b2f571b78988a654
Zeek <mzulkiful@yahoo.com>
2022-09-14 20:18:25 +0100

    The Dependency Array

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -5,12 +5,12 @@ import './TripList.css'
 export default function TripList(){
 
     const [trips, setTrips] = useState([])
-
+    const [url, setUrl] = useState('http://localhost:3000/trips')
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
+        fetch(url)
         .then(response => response.json())
         .then(json => setTrips(json))
-    }, [])
+    }, [url])
 
     console.log(trips)
 
@@ -26,6 +26,14 @@ export default function TripList(){
                         </li>
                 ))}
             </ul>
+            <div className="filters">
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+                    European Trips
+                </button>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>
+                    All Trips
+                </button>
+            </div>
         </div>
     )
 }
