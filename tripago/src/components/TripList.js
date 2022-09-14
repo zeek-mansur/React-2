4ad1e3d766b0bdfdb43d95eef1a90e6035d42572
Zeek <mzulkiful@yahoo.com>
2022-09-14 21:39:04 +0100

    Making a custom Fetch Hook

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,31 +1,23 @@
-import { useState, useEffect, useCallback } from "react"
+import { useState, } from "react"
+import { useFetch } from './hooks/useFetch'
 //Styles
 import './TripList.css'
 
 export default function TripList(){
 
-    const [trips, setTrips] = useState([])
+   
     const [url, setUrl] = useState('http://localhost:3000/trips')
+    const {data: trips } = useFetch(url)
     
-    const fetchTrips =  useCallback(async () => {
-
-        const response = await fetch(url)
-        const json = await response.json()
-        setTrips(json)
-    },[url])
-    
-    useEffect(() => {
-        fetchTrips()
-    }, [fetchTrips])
-
-    console.log(trips)
+   
+   // console.log(trips)
 
     return (
 
         <div className="trip-list">
             <h2>Trip List</h2>
             <ul>
-                {trips.map(trip =>(
+                {trips && trips.map(trip =>(
                         <li key={trip.id}>
                             <h3>{trip.title}</h3>
                             <p>{trip.price}</p>
